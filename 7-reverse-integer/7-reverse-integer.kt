8be415ac104e2ffa02f96c3c103d9c9f10034778
Timothy Serem <theecodepoet@gmail.com>
2022-03-21 20:05:23 +0300

    Time: 231 ms (56.55%), Space: 34 MB (75.28%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun reverse(x: Int): Int {
+        // if input length is 1
+        if (x.toString().length == 1) return x
+        var reversedInt = 0 // hold reversed number
+        var num = x // hold our input
+        while (num != 0) { // run this loop until value of num is not 0
+            val pop = num % 10
+            num /= 10 // our break point
+            // overflow checks
+            if (reversedInt > Integer.MAX_VALUE/10 || (reversedInt == Integer.MAX_VALUE / 10 && pop > 7)) return 0
+            if (reversedInt < Integer.MIN_VALUE/10 || (reversedInt == Integer.MIN_VALUE / 10 && pop < -8)) return 0
+            reversedInt = reversedInt * 10 + pop // take the rightmost digit of num and add it to reversedInt
+        }
+        return reversedInt
+    }
+}
