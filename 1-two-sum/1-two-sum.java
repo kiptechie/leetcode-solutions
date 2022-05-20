e6766042d53ca0edc4caf60dc98e7a323712e51d
Timothy Serem <theecodepoet@gmail.com>
2022-05-20 15:27:47 +0300

    Time: 4 ms (68.60%), Space: 46.1 MB (20.78%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -6,10 +6,8 @@ class Solution {
             if(hm.containsKey(target - nums[i])) {
                 int ind1 = i;
                 int ind2 = hm.get(target - nums[i]);
-                
                 ans[0] = ind1;
                 ans[1] = ind2;
-                
                 return ans; 
             } else {
                 hm.put(nums[i], i);
