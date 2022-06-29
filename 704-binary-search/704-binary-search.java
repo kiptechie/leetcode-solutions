3686d59e851f62876c170ee252d315e878072c85
Timothy Serem <theecodepoet@gmail.com>
2022-06-29 23:22:46 +0300

    Time: 0 ms (100.00%), Space: 54.6 MB (26.18%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -5,14 +5,13 @@ class Solution {
         while(left <= right) {
             int mid = left + (right - left) / 2;
             int curr = nums[mid];
-            System.out.println(curr);
             if (curr == target) {
                 return mid;
             }
-            if (curr > target) {
-                right = mid - 1;
-            } else {
+            if (curr < target) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
