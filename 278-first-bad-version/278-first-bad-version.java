b359b3d15548594e20c6505ae0d4506e5d30e28e
Timothy Serem <theecodepoet@gmail.com>
2022-06-29 23:31:46 +0300

    Time: 20 ms (47.92%), Space: 40.6 MB (64.43%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,26 +3,18 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        // Brute force
-        // List<Integer> badVersions = new ArrayList<>();
-        // for (int i = 1; i < n; i++) {
-        //     if (isBadVersion(i)) {
-        //         badVersions.add(i);
-        //     }
-        // }
-        
-        // Binary search
         int left = 1;
         int right = n;
+        int boundary = -1;
         while(left <= right) {
             int mid = left + (right - left) / 2;
-            System.out.println(mid);
             if (isBadVersion(mid)) {
                 right = mid - 1;
+                boundary = mid;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return boundary;
     }
 }
