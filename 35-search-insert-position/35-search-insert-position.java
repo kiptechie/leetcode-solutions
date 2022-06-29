80acf4a12f31e008d1fd9260579d587d01ab4676
Timothy Serem <theecodepoet@gmail.com>
2022-06-29 23:52:19 +0300

    Time: 0 ms (100.00%), Space: 43.5 MB (42.37%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,19 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int lastNum = nums[nums.length - 1];
-        if (target > lastNum) {
-            return nums.length;
-        }
-        if (target == lastNum) {
-            return nums.length - 1;
-        }
-        int firstNum = nums[0];
-        if (target < firstNum) {
-            return 0;
-        }
         int left = 0;
         int right = nums.length - 1;
-        int wouldBe = -1;
         while(left <= right) {
             int mid = left + (right - left) / 2;
             int curr = nums[mid];
@@ -25,11 +13,7 @@ class Solution {
             } else {
                 right = mid - 1;
             }
-            // check if target is within the range of current number and previous number
-            if (target < curr && mid > 0 && target > nums[mid - 1]) {
-                wouldBe = mid;
-            }
         }
-        return wouldBe;
+        return left;
     }
 }
